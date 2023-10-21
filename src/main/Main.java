60e20693e4ad4c47aca1e40c02ce6b43e96af5d9
dinoosauroYS <felipehfigueiredo@gmail.com>
2023-10-21 17:03:44 -0300

    testando as funcionalidades

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,10 +7,10 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-    System.out.println("Qual seu nome seu otario?");
+    System.out.println("Qual seu nome?");
     String nome = sc.nextLine();
     
-    System.out.println("Qual sua idade otario?");
+    System.out.println("Qual sua idade?");
 	int idade = sc.nextInt();
 	
 	System.out.println("Qual sua altura?");
